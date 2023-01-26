c836193fe5726d0049ed4983608b33a26e7bf331
girnal82 <grinal82@gmail.com>
2023-01-26 22:58:47 +0300

    minor linter changes

--- slider/task.js
+++ slider/task.js
@@ -1,20 +1,21 @@
 (() => {
     let currentSlide = 0;
     const sliderPrevious = document.body.querySelectorAll(
-        ".slider__arrow_prev");
+        ".slider__arrow_prev"
+    );
     console.log(sliderPrevious);
     sliderPrevious.onlick = function () {
         const menu = document.body.querySelectorAll(".slider__item");
         console.log(menu);
         if (currentSlide == 0) {
-            menu[currentSlide].classList.remove('slider__item_active');
+            menu[currentSlide].classList.remove("slider__item_active");
             currentSlide = 4;
-            menu[currentSlide].classList.add('slider__item_active')
+            menu[currentSlide].classList.add("slider__item_active");
         } else {
             currentSlide -= 1;
-            menu[currentSlide].classList.add('slider__item_active');
+            menu[currentSlide].classList.add("slider__item_active");
             let current = currentSlide + 1;
-            menu[current].classList.remove('slider__item_active');
+            menu[current].classList.remove("slider__item_active");
         }
     };
 
@@ -22,15 +23,15 @@
     sliderNext.onlick = function () {
         const menu = document.body.querySelectorAll(".slider__item");
         if (currentSlide >= 0) {
-            menu[currentSlide].classList.remove('slider__item_active');
+            menu[currentSlide].classList.remove("slider__item_active");
             if (currentSlide == menu.length - 1) {
                 currentSlide = 0;
-                menu[currentSlide].classList.add('slider__item_active');
+                menu[currentSlide].classList.add("slider__item_active");
             } else {
                 currentSlide += 1;
-                menu[currentSlide].classList.add('slider__item_active');
+                menu[currentSlide].classList.add("slider__item_active");
                 let current = currentSlide - 1;
-                menu[current].classList.remove('slider__item_active');
+                menu[current].classList.remove("slider__item_active");
             }
         }
     };
